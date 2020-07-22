72941230fd575c93009a5a952ad01bbaa7f26398
titarakshay <titarakshay@gmail.com>
2020-07-22 21:55:50 +0530

    app-done

--- index.js
+++ index.js
@@ -1,199 +1,212 @@
-let add = document.querySelector(".add");
-let all = document.querySelector(".all");
-let active = document.querySelector(".active");
-let completed = document.querySelector(".completed");
-let clear = document.querySelector(".clearcompleted");
-let ul = document.querySelector(".list");
-let footer = document.querySelector(".footer");
-let arrow= document.querySelector('.img');
-let left=document.querySelector('.left');
-let store = Redux.createStore(reducer);
+const add = document.querySelector(".add");
+const all = document.querySelector(".all");
+const active = document.querySelector(".active");
+const completed = document.querySelector(".completed");
+const clear = document.querySelector(".clearcompleted");
+const ul = document.querySelector(".list");
+const arrow = document.querySelector(".img");
+const itemsLeft = document.querySelector(".left");
 
-function reducer(state = { list: [], tab: "all" }, action) {
+const ADD_TODO = "ADD_TODO";
+const DELETE_TODO = "DELETE_TODO";
+const TOGGLE_TODO = "TOGGLE_TODO";
+const CLEAR_COMPLETED = "CLEAR_COMPLETED";
+const EDIT_TODO = "EDIT_TODO";
+const ARROW_SELECT = "ARROW_SELECT";
+const CHANGE_TAB = "CHANGE_TAB";
+
+const ACTIVE = "ACTIVE";
+const COMPLETE = "COMPLETE";
+
+let initialState = {
+  allTodo: [],
+  activeTab: "All",
+};
+
+let rootReducer = Redux.combineReducers({
+  allTodo: allTodoReducer,
+  activeTab: tabReducer,
+});
+
+let store = Redux.createStore(rootReducer);
+
+function allTodoReducer(state = initialState.allTodo, action) {
   switch (action.type) {
-    case "ADD_Todo": {
-      const newTodo = {
-        id: Date.now(),
-        text: action.text,
-        isDone: false,
-      };
-      return {...state,list:state.list.concat(newTodo)};
-    }
-    case "Delete_Todo": {
-      return {...state,list:state.list.filter((todo) => !(todo.id == action.id))};
-    }
-    case "Toggle_Todo": {
-      return {...state,list:state.list.map((todo) => {
-        if (todo.id === action.id) {
+    case ADD_TODO:
+      return [
+        ...state,
+        { id: Date.now(), text: action.payload, isDone: false },
+      ];
+    case DELETE_TODO:
+      return state.filter((todo) => todo.id !== action.payload);
+
+    case TOGGLE_TODO:
+      return state.map((todo) => {
+        if (todo.id === action.payload) {
           todo.isDone = !todo.isDone;
         }
         return todo;
-      })};
+      });
+    case CLEAR_COMPLETED: {
+      return state.filter((todo) => !todo.isDone);
     }
-    case "All_Todo": {
-      return { ...state, tab: "all" };
-    }
-    case "Active_Todo": {
-      return { ...state, tab: "active" };
-    }
-    case "Completed_Todo": {
-      return { ...state, tab: "complete" };
-    }
-    case "CLEAR_COMPLETED":{
-        return {...state,list:state.list.filter(todo=> !todo.isDone)}
-    }
-    case 'EDIT_TODO':{
-      return{...state,list:state.list.map(todo=>{
-        if(todo.id == action.load.id){
-          todo.text = action.load.text
-          return todo
+    case EDIT_TODO: {
+      return state.map((todo) => {
+        if (todo.id == action.load.id) {
+          todo.text = action.load.text;
+          return todo;
         }
-        return todo
-      })}
+        return todo;
+      });
     }
-    case 'ARROW_SELECT':{
-     newList=state.list.filter(todo=> !todo.isDone)
-     if(newList.length >0){
-         console.log('we are in true')
-         
-        return{...state,list:state.list.map(todo=>{
-            todo.isDone=true
-            return todo
-            
-         })}
-     }else{
-        console.log('we are in false')
-
-         return{...state,list:state.list.map(todo=>{
-             todo.isDone=false
-             return todo
-         })}
-     }
-      
+    case ARROW_SELECT: {
+      newList = state.filter((todo) => !todo.isDone);
+      if (newList.length > 0) {
+        return state.map((todo) => {
+          todo.isDone = true;
+          return todo;
+        });
+      } else {
+        return state.map((todo) => {
+          todo.isDone = false;
+          return todo;
+        });
+      }
     }
+    default:
+      return state;
   }
 }
 
-handleEdit=(event,id)=>{
-  let text=event.target.innerText;
-  const input=document.createElement('input');
-  input.classList.add('edit-input')
-  input.value=text
-  event.target.parentElement.replaceChild(input,event.target)
-  input.addEventListener('keyup',(event)=>{
-    if(event.keyCode == 13){
-      store.dispatch({
-        type:'EDIT_TODO',
-        load:{
-          id,
-          text:input.value
-        }
-      })
-    }
-  })
+function tabReducer(state = initialState.activeTab, action) {
+  switch (action.type) {
+    case CHANGE_TAB:
+      return action.payload;
+    default:
+      return state;
+  }
 }
 
-function createUi() {
-    ul.innerHTML = "";
-    const todos = store.getState();
-    console.log(store.getState(), "here");
-  let filterTodo= todos.list.filter(todo => {
-      if(todos.tab == "active" && todo.isDone == false) {
-          return todo
+const handleEdit = (event, id) => {
+  let text = event.target.innerText;
+  const input = document.createElement("input");
+  input.classList.add("edit-input");
+  input.value = text;
+  event.target.parentElement.replaceChild(input, event.target);
+  input.focus();
 
-      }else if(todos.tab == "complete" && todo.isDone == true) {
-        return todo
-
-    }else if(todos.tab== 'all'){
-        return todo
+  input.addEventListener("keyup", (event) => {
+    if (event.keyCode == 13) {
+      store.dispatch({
+        type: EDIT_TODO,
+        load: {
+          id,
+          text: input.value,
+        },
+      });
     }
-  })
-  console.log(filterTodo,"filttr")
-  leftList=filterTodo.filter(todo=> !todo.isDone)
-  left.innerHTML=`${leftList.length} items left`
+  });
+  input.addEventListener("blur", (event) => {
+    store.dispatch({
+      type: EDIT_TODO,
+      load: {
+        id,
+        text: input.value,
+      },
+    });
+  });
+};
+
+function filterTodo(tab, todos) {
+  switch (tab) {
+    case ACTIVE:
+      return todos.filter((todo) => !todo.isDone);
+    case COMPLETE:
+      return todos.filter((todo) => todo.isDone);
+    default:
+      return todos;
+  }
+}
+
+function createUi(ul, filterTodo) {
+  ul.innerHTML = "";
+  leftList = filterTodo.filter((todo) => !todo.isDone);
+  itemsLeft.innerHTML = `${leftList.length} items left`;
   filterTodo.forEach((todo) => {
     let li = document.createElement("li");
-    let p = document.createElement("p");
-    let spanX = document.createElement("span");
-    let checkInput = document.createElement("input");
-    checkInput.type = "checkbox";
-    checkInput.checked = todo.isDone;
-    p.classList.add("para");
     li.classList.add("li_styles");
+    let p = document.createElement("p");
+    p.classList.add("para");
+    p.innerHTML = todo.text;
+    p.addEventListener("dblclick", () => handleEdit(event, todo.id));
+    let spanX = document.createElement("span");
     spanX.className = "remove_items";
     spanX.innerHTML = "X";
     spanX.addEventListener("click", () => {
       store.dispatch({
-        type: "Delete_Todo",
-        id: todo.id,
+        type: DELETE_TODO,
+        payload: todo.id,
       });
     });
+    let checkInput = document.createElement("input");
+    checkInput.type = "checkbox";
+    checkInput.checked = todo.isDone;
     if (todo.isDone) p.style.textDecoration = "line-through";
     checkInput.addEventListener("click", () => {
-      console.log(todo, "todo");
       store.dispatch({
-        type: "Toggle_Todo",
-        id: todo.id,
-        isDone: todo.isDone,
+        type: TOGGLE_TODO,
+        payload: todo.id,
       });
     });
-    p.innerHTML = todo.text;
-    p.addEventListener('dblclick',()=>handleEdit(event,todo.id))
     li.append(checkInput, p, spanX);
     ul.append(li);
   });
-  // if (todos.length >= 1) {
-  // 	footer.style.display = "inline-block";
-  // } else {
-  // 	footer.style.display = "none";
-  // }
 }
 
-store.subscribe(createUi);
+store.subscribe(() =>
+  createUi(ul, filterTodo(store.getState().activeTab, store.getState().allTodo))
+);
 
 add.addEventListener("keyup", (event) => {
   if (event.keyCode === 13 && event.target.value.trim() !== "") {
     const text = event.target.value;
     store.dispatch({
-      type: "ADD_Todo",
-      text,
+      type: ADD_TODO,
+      payload: text,
     });
     event.target.value = "";
   }
 });
 
 all.addEventListener("click", (event) => {
-  console.log("all");
   store.dispatch({
-    type: "All_Todo",
+    type: CHANGE_TAB,
+    payload: "ALL",
   });
 });
 
 active.addEventListener("click", (event) => {
-  console.log("active");
   store.dispatch({
-    type: "Active_Todo",
+    type: CHANGE_TAB,
+    payload: ACTIVE,
   });
 });
 
 completed.addEventListener("click", (event) => {
-  console.log("complete");
   store.dispatch({
-    type: "Completed_Todo",
+    type: CHANGE_TAB,
+    payload: COMPLETE,
   });
 });
 
+clear.addEventListener("click", (event) => {
+  store.dispatch({
+    type: CLEAR_COMPLETED,
+  });
+});
 
-clear.addEventListener('click',(event)=>{
-    console.log('clear')
-    store.dispatch({
-        type:'CLEAR_COMPLETED'
-    })
-})
-
-arrow.addEventListener('click',(event)=>{
-    console.log('arrow')
-    store.dispatch({
-        type:'ARROW_SELECT'
-    })
-})
+arrow.addEventListener("click", (event) => {
+  store.dispatch({
+    type: ARROW_SELECT,
+  });
+});
